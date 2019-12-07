f41e23449b3093f4833bfc6496c40e7e5d612a47
Andika Andriana <andriana.mia1@gmail.com>
2019-12-07 14:46:42 +0700

    Tugas 14 Javascript
    Tugas ini Saya kumpulkan untuk Niomic.

---
+++ tugas14.js
@@ -0,0 +1,12 @@
+const tugas14 = () => {
+  var tglLahir = 5;
+  var pembagian = 37 / tglLahir;
+
+  console.log(pembagian);
+  console.log("---")
+  console.log("Round: ", Math.round(pembagian));
+  console.log("Ceil: ", Math.ceil(pembagian));
+  console.log("Floor: ", Math.floor(pembagian));
+}
+
+tugas14();
